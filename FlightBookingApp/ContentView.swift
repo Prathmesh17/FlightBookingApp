2d376f34dc7916b4049b4371326c89053c0fc04b
Prathmesh <prathmeshparteki@gmail.com>
2025-08-28 16:43:00 +0530

    Initial Commit

---
+++ FlightBookingApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlightBookingApp
+//
+//  Created by Prathmesh Parteki on 28/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlightBookingApp/FlightBookingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightBookingAppApp.swift
+//  FlightBookingApp
+//
+//  Created by Prathmesh Parteki on 28/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FlightBookingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
